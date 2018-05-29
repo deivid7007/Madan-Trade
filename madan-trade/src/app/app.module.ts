09419f7cd4616058887e92524572e53c899a46c3
deivid7007 <7deividmladenov007@gmail.com>
2018-05-29 22:17:37 +0300

    Added carouser in the project

--- madan-trade/src/app/app.module.ts
+++ madan-trade/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
