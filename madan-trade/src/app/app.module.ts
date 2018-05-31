75e855c26afc830c4853774e3ea6d3790f5a91da
deivid7007 <7deividmladenov007@gmail.com>
2018-05-31 17:44:34 +0300

    Implemented rounting between the components

--- madan-trade/src/app/app.module.ts
+++ madan-trade/src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { AdsComponent } from './components/ads/ads.component';
 
-
+const appRoutes: Routes = [
+{ path: '', redirectTo: '/home', pathMatch: 'full'},
+{ path: 'home', component: HomeComponent},
+{ path: 'about', component: AboutComponent },
+{ path: 'contacts', component: ContactsComponent },
+{ path: 'ads', component: AdsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CarouselComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactsComponent,
+    AdsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
