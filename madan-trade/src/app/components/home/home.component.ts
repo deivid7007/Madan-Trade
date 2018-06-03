fa4c466f40aca0ecf9d905724def25e91e973358
deivid7007 <7deividmladenov007@gmail.com>
2018-06-03 16:11:07 +0300

    Added Kinvey in the NoSQL DB conncetion in the project

--- madan-trade/src/app/components/home/home.component.ts
+++ madan-trade/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Kinvey } from 'kinvey-angular2-sdk';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+Kinvey.ping()
+  .then(function(response: {}) {
+    console.log('Kinvey Ping Success. Kinvey Service is alive, version: ');
+  }).catch(function(error) {
+    console.log('Kinvey Ping Failed. Response: ' + error.description);
+  });
   }
 
 }
--- madan-trade/src/main.ts
+++ madan-trade/src/main.ts
@@ -4,6 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Kinvey } from 'kinvey-angular2-sdk';
+
+Kinvey.init({
+  appKey: 'kid_SJ8Dd5lem',
+  appSecret: '04f3cfabca71499a97e6de4ecc246e92'
+});
+
 if (environment.production) {
   enableProdMode();
 }
