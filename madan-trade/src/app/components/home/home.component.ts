125b12a517fe4f163ff5919aff4b09a4e969e34e
deivid7007 <7deividmladenov007@gmail.com>
2018-06-07 16:08:54 +0300

    Cleared the Kinvey implementation

--- madan-trade/src/app/components/home/home.component.ts
+++ madan-trade/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Kinvey } from 'kinvey-angular2-sdk';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,12 +12,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     
-Kinvey.ping()
-  .then(function(response: {}) {
-    console.log('Kinvey Ping Success. Kinvey Service is alive, version: ');
-  }).catch(function(error) {
-    console.log('Kinvey Ping Failed. Response: ' + error.description);
-  });
   }
 
 }
--- madan-trade/src/main.ts
+++ madan-trade/src/main.ts
@@ -2,14 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-import { Kinvey } from 'kinvey-angular2-sdk';
-
-Kinvey.init({
-  appKey: 'kid_SJ8Dd5lem',
-  appSecret: '04f3cfabca71499a97e6de4ecc246e92'
-});
+import { environment } from './environments/environment'
 
 if (environment.production) {
   enableProdMode();
