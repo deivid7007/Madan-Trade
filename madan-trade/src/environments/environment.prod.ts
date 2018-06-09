cea5133c55ed15e56013200659b760c530e5a8de
deivid7007 <7deividmladenov007@gmail.com>
2018-06-09 15:51:56 +0300

    Added production env

--- madan-trade/src/environments/environment.prod.ts
+++ madan-trade/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDGWKtNSQc_pc6kn5BtDPIiPZuHfJdMRuM",
+    authDomain: "madan-trade-46658.firebaseapp.com",
+    databaseURL: "https://madan-trade-46658.firebaseio.com",
+    projectId: "madan-trade-46658",
+    storageBucket: "madan-trade-46658.appspot.com",
+    messagingSenderId: "997704338374"
+  }
 };
